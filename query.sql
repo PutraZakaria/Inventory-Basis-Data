e848c2e508c9cc115b7b31f6705bf46c61949c9a
PutraZakaria <117093982+PutraZakaria@users.noreply.github.com>
2023-11-20 11:41:20 +0700

    .

--- query.sql
+++ query.sql
@@ -1,3 +1,6 @@
+-- Active: 1683523504910@@127.0.0.1@3306
+CREATE DATABASE [InventoryJTI];
+
 
 select *
 from sys.databases;
@@ -7,7 +10,7 @@ USE [master];
 IF EXISTS(SELECT * FROM sys.databASes WHERE name = 'InventoryJTI')
     DROP DATABASE [InventoryJTI];
 
-IF NOT EXISTS (SELECT * FROM sys.databASes WHERE name = 'InventoryJTI')
-    CREATE DATABASE [InventoryJTI];
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'InventoryJTI')
+    
 
 USE [InventoryJTI];
